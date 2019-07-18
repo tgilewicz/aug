9e22b17f35159001a6c975ce850629b308720ba9
Tomasz Gilewicz <tomasz.gilewicz@cta.ai>
2019-07-18 13:20:09 +0200

    Add the operation responsible for filtering invalid annotations.

--- aug/core/operation.py
+++ aug/core/operation.py
@@ -4,8 +4,11 @@ import aug
 class Operation(aug.Pipeline):
 
     def apply_on_image(self, image):
-        """Runs all ops in the pipeline sequentially. """
-        raise NotImplementedError
+        """Apply transformations on image.
+            An image is not modified by default. If operation requires an image modification
+             then this method should be overwritten.
+        """
+        return image
 
     def apply(self, sample):
         image = self.apply_on_image(sample.image)
@@ -18,14 +21,14 @@ class Operation(aug.Pipeline):
 
     def apply_on_annotations(self, annotations):
         """Apply transformations on annotations.
-            Annotations are not modified by default. If operation requires modification
+            Annotations are not modified by default. If operation requires a modification
                 of annotations then this method should be overwritten.
         """
         return annotations
 
     def apply_on_masks(self, masks):
         """Apply transformations on mask.
-            Mask is not modified by default. If operation requires modification
-                of mask then this method should be overwritten.
+            A mask is not modified by default. If operation requires a mask modification
+             then this method should be overwritten.
         """
         return masks
